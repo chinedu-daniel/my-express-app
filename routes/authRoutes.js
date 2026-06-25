9a1a91109bc9b45bc68cbe5955ab31e7fb840023
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2026-06-25 21:18:48 +0100

    added the middleware to the route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { validate } = require("../middleware/validate");
 const registerUser = require("../controllers/authController");
-const { registerSchema } = require("../validators/authSchema");
+const validateRegister = require("../middleware/validateRegister");
 
 router.post("/register",
-    validate(registerSchema),
+    validateRegister,
     registerUser
 );
 
