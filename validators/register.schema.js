e90a2b5474c34b0d576ec4cf1dc2c236395d638d
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2026-06-25 21:16:50 +0100

    added the validator

---
+++ validators/register.schema.js
@@ -0,0 +1,11 @@
+const Joi = require("joi");
+
+const registerSchema = Joi.object ({
+    username: Joi.string().min(3).required(),
+
+    email: Joi.string().email().required(),
+
+    password: Joi.string().min(6).required()
+});
+
+module.exports = registerSchema;
