54f27713e469828134b7e187c336860ac296180c
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2026-06-27 17:38:36 +0100

    added a global error handling

--- middleware/validate.js
+++ middleware/validate.js
@@ -4,9 +4,13 @@ function validate(schema) {
         const { error } = schema.validate(req.body);
         
         if (error) {
-            return res.status(400).json({
-                message: error.details[0].message
-            });
+            // return res.status(400).json({
+            //     message: error.details[0].message
+            // });
+
+            const error = new Error("Email is required");
+
+            return next(error);
         }
         
         next();
