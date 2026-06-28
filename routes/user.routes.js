8e15b60a35800dbcbb4502da6be8dffece6968a9
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2026-06-28 19:18:31 +0100

    added the user routes

---
+++ routes/user.routes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+
+const registerUser = require("../controllers/user.controller");
+const validate = require("../middleware/validate");
+const registerSchema = require("../validators/register.schema");
+const errorHandler = require("../middleware/error.middleware");
+
+router.post(
+    "/register",
+    validate(registerSchema),
+    registerUser
+);
+
+router.use(errorHandler);
+
+module.exports = router;
