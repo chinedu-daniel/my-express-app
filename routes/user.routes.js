51046284620b121550431ef0d02537845b98222d
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2026-07-05 12:09:01 +0100

    added the signup schema

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,16 +2,18 @@ const express = require("express");
 const router = express.Router();
 
 const userController = require("../controllers/user.controller");
+const validate = require("../middleware/validate");
+const { signupSchema } = require("../validators/user.schema");
 
-console.log("BODY:", req.body);
-// const validate = require("../middleware/validate");
+
+// console.log("BODY:", req.body);
 // const registerSchema = require("../validators/register.schema");
 // const errorHandler = require("../middleware/error.middleware");
 
 router.post(
-    "/users",
-    // validate(registerSchema),
-    userController.createUser
+    "/users/signup",
+    validate(signupSchema),
+    userController.signup
 );
 
 // router.use(errorHandler);
