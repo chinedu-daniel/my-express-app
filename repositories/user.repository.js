493ab114cf4a710188d636ac92e68e7b1ee08aab
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2026-07-05 19:37:04 +0100

    find user by id

--- repositories/user.repository.js
+++ repositories/user.repository.js
@@ -11,8 +11,21 @@ exports.findUserByEmail = async (email) => {
 
 exports.createUser = async ({ name, email, password }) => {
   const result = await db.query(
-    `INSERT INTO users (email, name, password) VALUES ($1, $2, $3) RETURNING id, name, email`,
+    `INSERT INTO users
+    (email, name, password) VALUES ($1, $2, $3) 
+    RETURNING id, name, email`,
     [email, name, password]
+  );
+
+  return result.rows[0];
+};
+
+exports.findUserById = async (id) => {
+  const result = await db.query(
+    `SELECT id, name, email
+    FROM users
+    WHERE id = $1`,
+    [id]
   );
 
   return result.rows[0];
