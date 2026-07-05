55d0019e53b514b4603eaa758edecc0583a63710
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2026-07-05 13:15:48 +0100

    added login schema

--- validators/user.schema.js
+++ validators/user.schema.js
@@ -14,4 +14,14 @@ const signupSchema = Joi.object ({
     .required()
 });
 
-module.exports = { signupSchema };
+const loginSchema = Joi.object({
+    email: Joi.string()
+    .email()
+    .required(),
+
+    password: Joi.string()
+    .min(6)
+    .required()
+});
+
+module.exports = { signupSchema, loginSchema };
