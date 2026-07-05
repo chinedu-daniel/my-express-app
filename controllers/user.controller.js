aaf27ab06c6076b6fed8ed129024eb72b37f8c5b
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2026-07-05 13:11:41 +0100

    added the login controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -11,3 +11,14 @@ exports.signup = asyncHandler(async (req, res) => {
     data: user,
   });
 });
+
+exports.login = asyncHandler(async (req, res) => {
+  console.log("BODY:", req.body);
+
+  const  user = await userService.login(req.body);
+
+  res.status(200).json({
+    message: "User logged in successfully",
+    data: user
+  });
+});
