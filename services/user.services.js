71516f3e5336d1b35308675383cda1db917e5e12
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2026-07-07 07:49:42 +0100

    token and generate token

--- services/user.services.js
+++ services/user.services.js
@@ -44,7 +44,8 @@ exports.login = async (data) => {
         user: {
             id: user.id,
             name: user.name,
-            email: user.email
+            email: user.email,
+            role: user.role
         },
         token
     };
