7350ab090a22f99701548f9486e1c08b91ebbfcc
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2026-07-07 07:48:30 +0100

    added the logout and admin-only route

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -5,6 +5,7 @@ const userController = require("../controllers/user.controller");
 const validate = require("../middleware/validate");
 const { signupSchema, loginSchema } = require("../validators/user.schema");
 const protect = require("../middleware/auth.middleware");
+const authorize = require("../middleware/authorize.middleware");
 
 // const errorHandler = require("../middleware/error.middleware");
 
@@ -14,7 +15,7 @@ router.post(
     userController.signup
 );
 
-router.post(
+ router.post(
     "/users/login",
     validate(loginSchema),
     userController.login
@@ -24,6 +25,19 @@ router.get(
     "/users/profile", 
     protect,
     userController.getProfile
+);
+
+router.post(
+    "/users/logout",
+    protect, 
+    userController.logout
+);
+
+router.get(
+    "/users/admin-only",
+    protect,
+    authorize("admin"),
+    userController.adminOnly
 );
 
 // router.use(errorHandler);
