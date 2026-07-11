2cdc16aac4798752d2a855f67c76350193bd555e
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2026-07-11 08:15:00 +0100

    generate reset token function

---
+++ utils/generateResetToken.js
@@ -0,0 +1,19 @@
+const crypto = require("crypto");
+
+function generateResetToken() {
+    const rawToken = crypto
+        .randomBytes(32)
+        .toString("hex");
+
+    const hashedToken = crypto
+        .createHash("sha256")
+        .update(rawToken)
+        .digest("hex");
+
+    return {
+        rawToken,
+        hashedToken
+    };
+}
+
+module.exports = generateResetToken;
